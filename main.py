f533284ff2a27a7e11ab3b4975c1782d2ac8f190
addiGoat <calvinseikoblais@gmail.com>
2026-09-25 00:26:24 -0400

    initial card serializer and get_card_by_id workflow

--- main.py
+++ main.py
@@ -35,8 +35,7 @@ def load_board():
 
     return board
     
-def get_board_data() -> dict[str, Any]:
-    board = load_board()
+def get_board_data(board) -> dict[str, Any]:
 
     lists_data = []
     for list_index, trellist in enumerate(board.all_lists()):
@@ -48,7 +47,7 @@ def get_board_data() -> dict[str, Any]:
             for label in card.labels:
                 labels_data.append(serializers.serialize_label(label))
     
-            cards_data.append(serializers.serialize_card(card, card_index, labels_data))
+            cards_data.append(serializers.serialize_card_summary(card, card_index, labels_data))
     
         lists_data.append(serializers.serialize_list(trellist, list_index, cards_data))
 
@@ -57,13 +56,60 @@ def get_board_data() -> dict[str, Any]:
 
     return data
 
+def get_card_by_id(id) -> dict[str, Any]:
+    card = client.get_card(id)
+
+    label_data = []
+    comments = []
+    
+    for label in card.labels:
+        label_data.append(serializers.serialize_label(label))
+    
+    for comment in card.get_comments():
+        comments.append(serializers.serialize_comment(comment))
+
+    card_data = serializers.serialize_card_detail(card, label_data, comments)
+
+    return card_data
+    
+
+
+
+    # "id": "card_id",
+    # "name": "Build a Trello desktop widget",
+    # "description": "Build a minimal Quickshell interface...",
+    # "url": "https://trello.com/c/...",
+    # "list": { "id": "list_id", "name": "Active" },
+    # "labels": [
+    #   { "id": "label_id", "name": "Quickshell", "color": "blue" }
+    # ],
+    # "comments": [
+    #   {
+    #     "id": "comment_id",
+    #     "text": "API authentication is working.",
+    #     "created_at": "2026-09-24T18:30:00Z",
+    #     "author": {
+    #       "id": "member_id",
+    #       "name": "Addi",
+    #       "avatar_url": "https://..."
+    #     }
+    #   }
+    # ]
 
 
 def main() -> int:
-    print(json.dumps({
-        "ok": True,
-        "data": get_board_data()
-        }))
+
+    
+    card = get_card_by_id("G0xi8dGB")
+
+
+    print(json.dumps(card))
+    # for comment in card.get_comments():
+    #     print(json.dumps(comment))
+    # print(json.dumps({
+    #     "ok": True,
+    #     "data": get_board_data(load_board())
+    #     }))
     return 0
 
 if __name__ == "__main__":
